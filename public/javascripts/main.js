f0d0537c3e943c21d3b4ee6470f1742d2ca4981f
Adam Woodbury <adamwoodbury@gmail.com>
2016-01-27 01:42:01 -0800

    Make everything just a little bit better.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,16 +1,35 @@
-$.getJSON('/json/cards.json', function(data){
-  var cards = TAFFY(data);
-  function write_card(index){
-    var card = cards().get()[index]
-    $('.front p').html(card.front)
-    $('.back p').html(card.back)
-    $('.meta').html('[ ' + card.category + ' ]')
-  }
-  write_card(Math.floor(Math.random() * cards().count()));
-  $('.flip').click(function(){
-    $('.flip').toggleClass('hover')
+(function () {
+  var $front = $('.front p');
+  var $back = $('.back p');
+  var $meta = $('.meta-content');
+  var $flip = $('.flip');
+  var $random = $('#random');
+
+  $.getJSON('/json/cards.json', function (response) {
+    var cards = response;
+
+    function updateCard(index) {
+      var card = cards[index];
+
+      $front.html(card.front);
+      $back.html(card.back);
+      $meta.html(card.category);
+    }
+
+    function randomIndex() {
+      return Math.floor(Math.random() * cards.length);
+    }
+
+    // make the initial card
+    updateCard(randomIndex());
+
+    // flip the card when a user clicks it
+    $flip.click(function () {
+      $flip.toggleClass('hover');
+    });
+
+    $random.click(function () {
+      updateCard(randomIndex());
+    });
   });
-  $('#random').click(function(){
-    write_card(Math.floor(Math.random() * cards().count()))
-  });
-});
+})();
